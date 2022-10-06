59c474668c8e670361c274e23ad479096bff16ea
thien <thientran20101@gmail.com>
2022-10-06 08:13:07 +0700

    test external modules #4

--- math.js
+++ math.js
@@ -1,18 +1,16 @@
 var moment = require('moment-timezone');
-
 var add = (num1, num2) => {
     return Number(num1 + num2);
 }
 var mul = (num1, num2) => {
     return Number(num1 * num2);
 }
-var addConsole = (num1, num2) => {
+export function addConsole(num1, num2) {
     return "Result : " + num1 + " + " + num2 + " = " + add(num1.num2);
 }
-var mulConsole = (num1, num2) => {
+export function mulConsole(num1, num2) {
     return "Result : " + num1 + " * " + num2 + " = " + mul(num1.num2);
 }
-var NYTime = () => {
+export function NYTime() {
     return "Time in New York is " + moment().tz('America/New_York').format('DD-MM-YYYY');
 }
-module.exports = { addConsole, mulConsole, NYTime }
